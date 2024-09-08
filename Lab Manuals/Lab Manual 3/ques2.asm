65030f56ff8ef84e18c113dc38c6ded886f3182a
Muhammad Mohad <l230784@lhr.nu.edu.pk>
2024-09-08 22:30:33 +0500

    ok

--- Lab Manuals/Lab Manual 3/ques2.asm
+++ Lab Manuals/Lab Manual 3/ques2.asm
@@ -1,26 +1,47 @@
 [org 0x0100]
 
-mov cx, 10
-mov si, 0
-mov di, 2
+mov cx, 10         
+mov si, 0        
+mov di, 2       
 
 elimination:
-	mov dx, 0
-	mov [array + si], dx
-	jmp aftereliminate
-	
-loop:
-	mov ax, [array + si]
-	mov bx, [array + di]
-	cmp ax, bx
-	je elimination
-	aftereliminate:
-	add si, 2
-	add di, 2
-	sub cx, 1
-	jnz loop
-	
-mov ax, 0x4c00
-int 0x21
+    mov dx, 0
+    mov [array + si], dx
+    jmp aftereliminate
 
-array: dw 2,2,2,3,4,4,5,5,5,6
+loop:
+    mov ax, [array + si] 
+    mov bx, [array + di]  
+    cmp ax, bx             
+    je elimination        
+aftereliminate:
+    add si, 2             
+    add di, 2            
+    sub cx, 1              
+    jnz loop              
+
+mov cx, 10         
+mov si, 0         
+mov di, 0         
+zero_pointer:
+    mov ax, [array + si]  
+    cmp ax, 0              
+    je next_element        
+    mov [array + di], ax   
+    add di, 2              
+next_element:
+    add si, 2              
+    loop zero_pointer      
+
+fill_zeros:
+    cmp di, 20             
+    je done                
+    mov word [array + di], 0 
+    add di, 2             
+    jmp fill_zeros        
+
+done:
+    mov ax, 0x4c00        
+    int 0x21
+
+array: dw 2, 2, 2, 3, 4, 4, 5, 5, 5, 6
